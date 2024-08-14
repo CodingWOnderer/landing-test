bc41a1e9d97e759080b952513a3c7650826a3ef5
CodingWOnderer <vipin@capcons.com>
2024-08-14 17:12:20 +0530

    extra mockup error solved

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,7 +6,6 @@ import InspirationSection from "@/components/inspiration-section";
 import AppSection from "@/components/app-section";
 import LatestInsightSection from "@/components/latest-insight-section";
 import StudioSection from "@/components/stydio-section";
-import AffiliateSection from "@/components/AffliatedSection";
 
 export default function Home() {
   return (
@@ -26,7 +25,6 @@ export default function Home() {
       <LatestInsightSection />
       <StudioSection />
       <AppSection />
-      <AffiliateSection />
     </div>
   );
 }
